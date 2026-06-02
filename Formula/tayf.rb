ea24aab80d133249e1b700007634a33365b3bea0
beraartuc <61538603+beraartuc@users.noreply.github.com>
2026-06-02 20:57:18 +0300

    tayf 0.12.1

--- Formula/tayf.rb
+++ Formula/tayf.rb
@@ -1,8 +1,8 @@
 class Tayf < Formula
   desc "PTY-based, terminal-agnostic, regex-driven output colorizer"
   homepage "https://github.com/beraartuc/tayf"
-  url "https://static.crates.io/crates/tayf/tayf-0.12.0.crate"
-  sha256 "51e34f30adb204548b3f302d7cc6cf60edf92a714542dfbd83a25a9aba515906"
+  url "https://static.crates.io/crates/tayf/tayf-0.12.1.crate"
+  sha256 "0d0ee7178da0fc0c335e03609a9e01e8c85d011424485f3b3181c793b0f9c882"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on "rust" => :build
